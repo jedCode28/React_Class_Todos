9a1ef6ceb51fc8d5a08a04f3a79a33822fc3ea1a
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-13 13:16:35 -0600

    updated todoForm

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 import List from './List';
 import TodoForm from './TodoForm';
 class App extends Component {
-  state = {
-    todos: [
-      { id: 1, name: "Learn Rails", complete: true },
-      { id: 2, name: "Learn React", complete: false },
-      { id: 3, name: "Get Job", complete: false },
-    ]
-  };
+  state = { todos: [] };
+
+  getUniqId = () => {
+    // This is only a helper function for id's since no db
+    return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+  }
+
+  addItem = (name) => {
+    const { todos } = this.state
+    const todo = { name, id: this.getUniqId(), complete: false }
+    this.setState({ todos: [todo, ...todos] });
+  }
 
   render() {
     const { todos } = this.state;
     return(
       <div>
-        <TodoForm />
-      <List name="Todo List" items={todos} />
+        <TodoForm addItem={this.addItem} />
+        <List name="Todo List" items={todos} />
       </div>
     );
   }
--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 class TodoForm extends React.Component {
+  state = { name: '' }
+
+  handleChange = (e) => {
+    this.setState({ name: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addItem(this.state.name);
+    this.setState({ name: '' })
+  }
+
   render() {
+    const { name } = this.state;
+
     return (
-      <form>
-        <input required placeholder="Add an Item" />
+      <form onSubmit={this.handleSubmit}>
+        <input
+          value={name}
+          name="name"
+          onChange={this.handleChange}
+         required 
+         placeholder="Add an Item" 
+        />
       </form>
     )
   }
