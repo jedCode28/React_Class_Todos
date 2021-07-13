ffd8afe38ea76f7b329fcdf092d0316ec23c0329
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-13 14:35:35 -0600

    complete except styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import List from './List';
 import TodoForm from './TodoForm';
+import Footer from './Footer';
 class App extends Component {
-  state = { todos: [] };
+  state = { todos: [], filter: 'All' };
+
+  setFilter = (filter) => {
+    this.setState({filter})
+  }
 
   getUniqId = () => {
     // This is only a helper function for id's since no db
@@ -32,12 +37,25 @@ class App extends Component {
     })
   }
 
+  visibleItems = () => {
+    const { todos, filter } = this.state;
+    switch(filter){
+      case 'Active':
+        return todos.filter( t => !t.complete )
+      case 'Complete': 
+        return todos.filter( t => t.complete ) 
+      default: 
+        return todos;
+    }
+  }
+
   render() {
-    const { todos } = this.state;
+    const { todos, filter } = this.state;
     return(
       <div>
         <TodoForm addItem={this.addItem} />
-        <List name="Todo List" items={todos} todoClick={this.handleClick} />
+        <List name="Todo List" items={this.visibleItems()} todoClick={this.handleClick} />
+        <Footer filter={filter} setFilter={this.setFilter} />
       </div>
     );
   }
