e08090f78b3cebe85dcf5006aba171ad48cb77d6
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-13 13:44:21 -0600

    app and list updated

--- src/List.js
+++ src/List.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Todo from './Todo';
 
 const List = ({ items, name }) => (
   <div>
     <h2>{name}</h2>
     <ul>
-      { items.map( item => <li key={item.id}>{item.name}</li>)}
+      { items.map( item => <Todo key={item.id} id={item.id} name={item.name} complete={item.complete} /> )}
     </ul>
   </div>
 );
